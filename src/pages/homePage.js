abbb746b826e6fe85afbb3d961ed925d99c30e6c
James O'Rourke <devbopo.joe@gmail.com>
2022-04-26 23:05:49 +0100

    Refactor to allow for new filtering hook, and add the hook.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getMovies } from "../api/tmdb-api";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
-  
-  const favourites =  movies.filter((m) => m.favourite);
-  console.log(favourites)
+  const favourites = movies.filter((m) => m.favourite);
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [],
+    [titleFiltering, genreFiltering]
+  );
+
   localStorage.setItem("favourites", JSON.stringify(favourites));
 
   const addToFavourites = (movieId) => {
@@ -16,6 +35,13 @@ const HomePage = (props) => {
     setMovies(updatedMovies);
   };
 
+  const changeFilterValues = (type, value) => {
+    const newf = { name: type, value: value };
+    const newFilters =
+      type === "title" ? [newf, filterValues[1]] : [filterValues[0], newf];
+    setFilterValues(newFilters);
+  };
+
   useEffect(() => {
     getMovies().then((movies) => {
       setMovies(movies);
@@ -23,12 +49,21 @@ const HomePage = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const displayedMovies = filterFunction(movies);
+
   return (
-    <PageTemplate
-      title="Discover Movies"
-      movies={movies}
-      selectFavourite={addToFavourites}
-    />
+    <>
+      <PageTemplate
+        title="Discover Movies"
+        movies={displayedMovies}
+        selectFavourite={addToFavourites}
+      />
+      <MovieFilterUI
+        filterInputChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
 export default HomePage;
